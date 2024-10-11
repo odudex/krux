8fbb034c503eb7770a2c2c9ce74ebd72315f8cbe
Odudex <eduardo.schoenknecht@gmail.com>
2024-10-11 15:39:26 -0300

    update to 24.10.beta4

--- android/src/krux/pages/__init__.py
+++ android/src/krux/pages/__init__.py
@@ -64,8 +64,10 @@ PROCEED = (BUTTON_ENTER, BUTTON_TOUCH)
 
 LETTERS = "abcdefghijklmnopqrstuvwxyz"
 UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-NUM_SPECIAL_1 = "0123456789 !#$%&'()*"
+NUM_SPECIAL_1 = "1234567890 !#$%&'()*"
 NUM_SPECIAL_2 = '+,-./:;<=>?@[\\]^_"{|}~'
+NUM_SPECIAL_3 = " !#$%&'()*"  # NUM_SPECIAL_1 without numbers
+DIGITS = "1234567890"
 
 BATTERY_WIDTH = 22
 BATTERY_HEIGHT = 7
@@ -146,12 +148,16 @@ class Page:
         """
         buffer = starting_buffer
         pad = Keypad(self.ctx, keysets, possible_keys_fn)
+        big_tittle = len(self.ctx.display.to_lines(title)) > 1
         while True:
             self.ctx.display.clear()
-            offset_y = DEFAULT_PADDING
-            if (len(buffer) + 1) * FONT_WIDTH < self.ctx.display.width():
+            offset_y = MINIMAL_PADDING if big_tittle else DEFAULT_PADDING
+            if lcd.string_width_px(buffer) < self.ctx.display.width():
                 self.ctx.display.draw_hcentered_text(title, offset_y)
-                offset_y += FONT_HEIGHT * 3 // 2
+                if big_tittle:
+                    offset_y += 2 * FONT_HEIGHT
+                else:
+                    offset_y += FONT_HEIGHT * 3 // 2
             self.ctx.display.draw_hcentered_text(buffer, offset_y)
 
             if progress_bar_fn:
@@ -159,6 +165,7 @@ class Page:
             pad.compute_possible_keys(buffer)
             pad.get_valid_index()
             pad.draw_keys()
+            pad.draw_keyset_index()
             btn = self.ctx.input.wait_for_button()
             if btn == BUTTON_TOUCH:
                 btn = pad.touch_to_physical()
